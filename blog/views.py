99fdc0dcef345e4b6f09e630b8c5f255130cf843
Nishant Bhandari <nishantbhandari@Nishants-MacBook-Air.local>
2025-08-07 16:27:31 +0530

    added template & static files

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+
+from django.urls import reverse
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
