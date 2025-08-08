3ac8752fedc501db9a33e383b06b65b66bd7fc51
Nishant Bhandari <nishantbhandari@Nishants-MacBook-Air.local>
2025-08-09 02:08:24 +0530

    added many htmls,css's then created posts page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"), #/blog/
-    path("/posts", views.posts, name="posts-page"),
-    path("/post/<slug:slug>", views.post_detail, name="post-detail-page") #/postsss/my-first-postss these are slug bro keep it in mind
+    path("posts", views.posts, name="posts-page"),
+    path("post/<slug:slug>", views.post_detail, name="post-detail-page") #/postsss/my-first-postss these are slug bro keep it in mind
 ]
